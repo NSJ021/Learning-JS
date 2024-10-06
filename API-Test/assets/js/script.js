0d4405fe51dd334f0c3a93bfc80dd9d88de98ffd
Nathan Jones <nsjones021@gmail.com>
2024-10-06 19:00:01 +0100

    added processOptions function and fixes

--- API-Test/assets/js/script.js
+++ API-Test/assets/js/script.js
@@ -1,86 +1,111 @@
 const API_KEY = "reFkzfQbZMET3Rbfg0xTQCKG1_c";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
-const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+const resultsModal = new bootstrap.Modal(
+  document.getElementById("resultsModal")
+);
 
-
-// Gettig elements form index.html via ID
-document.getElementById("status").addEventListener("click", e => getStatus(e));
-document.getElementById("submit").addEventListener("click", e => postForm(e));
+// Getting elements form index.html via ID
+document
+  .getElementById("status")
+  .addEventListener("click", (e) => getStatus(e));
+document.getElementById("submit").addEventListener("click", (e) => postForm(e));
 
 /**
  * async function to get the status of the API key
- * @param {*} e 
+ * @param {*} e
  */
 async function getStatus(e) {
-    const queryString = `${API_URL}?api_key=${API_KEY}`;
+  const queryString = `${API_URL}?api_key=${API_KEY}`;
 
-    const response = await fetch(queryString);
+  const response = await fetch(queryString);
 
-    const data = await response.json();
+  const data = await response.json();
 
-    if (response.ok) {
-        displayStatus(data);
-    } else {
-        throw new Error(data.error);
-    }
+  if (response.ok) {
+    displayStatus(data);
+  } else {
+    throw new Error(data.error);
+  }
 }
 
 /**
- * Function to display the status of the API key, linked to modal popup for check key button. 
- * @param {*} data 
+ * Function to display the status of the API key, linked to modal popup for check key button.
+ * @param {*} data
  */
 function displayStatus(data) {
+  let heading = "API Key Status";
+  let results = `<div>Your key is valid until</div>`;
+  results += `<div class="key-status">${data.expiry}</div>`;
 
-    let heading = "API Key Status";
-    let results = `<div>Your key is valid until</div>`;
-    results += `<div class="key-status">${data.expiry}</div>`;
+  document.getElementById("resultsModalTitle").innerText = heading;
+  document.getElementById("results-content").innerHTML = results;
 
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-
-    resultsModal.show();
+  resultsModal.show();
 }
 
 /**
  * async function for posting the form and its data
- * @param {*} e 
+ * @param {*} e
  */
 async function postForm(e) {
-    
-    const form = new FormData(document.getElementById("checksform"));
+  const form = processOptions(
+    new FormData(document.getElementById("checksform"))
+  );
 
-    const response = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-                    "Authorization": API_KEY,
-                 },
-                 body: form,
-        });
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      Authorization: API_KEY,
+    },
+    body: form,
+  });
 
-    const data = await response.json();
+  const data = await response.json();
 
-    if (response.ok) {
-        displayErrors(data);
-    } else {
-        throw new Error(data.error);
-    }
+  if (response.ok) {
+    displayErrors(data);
+  } else {
+    throw new Error(data.error);
+  }
 }
 
+/**
+ * Display errors function, setups content to run checks button, loops through all errors then displays them in a modal.
+ * @param {*} data
+ */
 function displayErrors(data) {
+  let heading = `JSHint Result for ${data.file}`;
 
-    let heading = `JSHint Result for ${data.file}`;
-
-    if (data.total_errors === 0){
-        result = `<div class="no_errors">No Errors Reported</div>`;
-    } else {
-        results = `<div>Total Errors <span class="error_count">${data.total_errors}</span></div>`;
-        for (let error of data.error_list) {
-            results += `<div>At line <span class="line">${error.line}</span>, `;
-            results += `column <span class="column">${error.col}</span></div>`;
-            results += `<div class="error">${error.error}</div>`;
-        }
+  if (data.total_errors === 0) {
+    result = `<div class="no_errors">No Errors Reported</div>`;
+  } else {
+    results = `<div>Total Errors <span class="error_count">${data.total_errors}</span></div>`;
+    for (let error of data.error_list) {
+      results += `<div>At line <span class="line">${error.line}</span>, `;
+      results += `column <span class="column">${error.col}</span></div>`;
+      results += `<div class="error">${error.error}</div>`;
     }
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-    resultsModal.show();
+  }
+  document.getElementById("resultsModalTitle").innerText = heading;
+  document.getElementById("results-content").innerHTML = results;
+  resultsModal.show();
+}
+
+/**
+ * Process Options function, creates an array, loops through all entries tags as options, pushes value of each entry into array, deletes old options and joins array to create string
+ * @param {*} form
+ * @returns
+ */
+function processOptions(form) {
+  let optArray = [];
+
+  for (let entry of form.entries()) {
+    if (entry[0] === "options") {
+      optArray.push(entry[1]);
+    }
+  }
+  form.delete("options");
+  form.append("options", optArray.join());
+
+  return form;
 }
